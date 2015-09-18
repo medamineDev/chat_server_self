929dae72525fd2401e68aaff5d74738eb2bfc2c1
medamineDev <grinemedamine3@gmail.com>
2015-09-18 15:07:51 +0100

    fixe msg paginator

--- nodejs/server.js
+++ nodejs/server.js
@@ -7,8 +7,8 @@ server.listen(8890);
 
 
 app.all('/*', function (req, res, next) {
-    // CORS headers
-    /* res.header('Access-Control-Allow-Origin', 'http://doctorcrm.dev:2772');*/
+    /*CORS headers */
+
     res.header("Access-Control-Allow-Origin", "*");
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', '*');
@@ -38,74 +38,24 @@ var client = new Client();
 var users = [];
 
 
-
-var disconnect = function(user){
+var disconnect = function (user) {
 
     var len = 0;
 
     for (var i = 0, len = users.length; i < len; ++i) {
         var p = users[i];
 
-        /*if (p.socket == socket.id) {
-            users.splice(i, 1);
-            break;
-        }*/
         if (p.id == user.id) {
-            console.log(p.id+" user already exist !");
+            console.log(p.id + " user already exist !");
             users.splice(i, 1);
             break;
         }
     }
 
-    //callback(true);
     users.push(user);
 
 }
 
-var is_user_connected = function (user) {
-
-    console.log("checking already connected users !" + users.length);
-
-
-    var user_exist = false;
-
- /*   var i = 0;
-    while (user[i]) {
-        var p = users[i];
-        console.log(user.id + " trying to connect , already connected IDs ---->" + p.id);
-
-        i++;
-    }*/
-
-    for (var i = 0, len = users.length; i < len; i++) {
-        var p = users[i];
-
-       console.log(user.id + " trying to connect , already connected IDs ---->" + p.id);
-
-        if (p.id) {
-
-            if (p.id == user.id) {
-                console.log(p.id + "--->" + user.id + "user already connected ----> disconnect");
-
-                users.splice(i, 1);
-                user_exist = true;
-
-
-            } else {
-                user_exist = false;
-
-
-            }
-
-        }
-
-
-
-    }
-
-    return user_exist;
-
-};
 
 io.sockets.on('connection', function (socket) {
 
@@ -116,18 +66,8 @@ io.sockets.on('connection', function (socket) {
         var user = new Object();
         user.id = user_id;
         user.socket = socket.id;
-
-       /* if (!is_user_connected(user)) {
-
-            console.log("new logged ID ----->" + user_id);
-            users.push(user);
-
-        }*/
-
+        /* Disconnect if the user is connected */
         disconnect(user);
-        //users.push(user);
-        //users.push(user);
-        //console.log("new user connected ");
 
 
     });
@@ -154,7 +94,7 @@ io.sockets.on('connection', function (socket) {
             data: {id_sender: id_user, id_receiver: logged_user_id, message_body: data},
             headers: {"Content-Type": "application/json"}
         };
-        /*http://chat.web.anypli.com/*/
+
         client.post("http://chat.web.anypli.com/send_msg_api", args, function (resp, response) {
 
             console.log(resp.msg);
